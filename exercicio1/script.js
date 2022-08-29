a0642f69a3f8883400abbe281b2e325a5baf0f3d
Casenrique <carloshenriquesouza.eng@gmail.com>
2022-08-29 19:09:50 -0300

    Feita nova branch e adicionados exercícios

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -4,8 +4,7 @@ let idade
 // console.log(idade, typeof idade)
 
 // Como as variáveis foram declaradas, mas não foi atribuído nenhum tipo específico de dado, 
-// mas sim deixado deliberadamente o valor como "null", 
-//as variáveis foram reconhecidas apenas como objetos que podem assumir valor posteriomente. 
+//as variáveis foram reconhecidas apenas como indefined que podem assumir valor posteriomente.  
 
 nome = prompt("Qual é o seu nome?")
 idade = Number(prompt("Qual é a sua idade?"))
@@ -13,7 +12,6 @@ idade = Number(prompt("Qual é a sua idade?"))
 console.log(nome, typeof nome)
 console.log(idade, typeof idade)
 
-// Notei que após declarar as variáveis e elas trocaram o tipo de objeto para
-// variáveis do tipo string e number, respectivamente.
+// Notei que após declarar as variáveis e elas trocaram o tipo de objeto para variáveis do tipo string e number, respectivamente.
 
 console.log(`Olá, ${nome}, você tem ${idade} anos`)
