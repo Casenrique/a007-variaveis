c48544ffa279536ae65b373f6084a10f16a23c0f
Casenrique <carloshenriquesouza.eng@gmail.com>
2022-08-29 16:07:20 -0300

    atualizando exercício 3, com comentários

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,8 +1,9 @@
-let a = 10
-let b = 25
-let c = a
-a = b
-b = c
+let a = 10 //defini valor 10 para a
+let b = 25 //defini valor 25 para b
+console.log("a:", a, "b:", b)
+let c = a // guarda o valor de a em c, assim c = 10
+a = b //redefine valor de a para 25, assumindo o mesmo valor de b
+b = c //redefine valor de b para 10, assumindo o mesmo valor de c, que havia guardado o valor de a
 console.log("O novo valor de a é", a)
 console.log("O novo valor de b é", b)
 console.log("O novo valor de c é", c)
